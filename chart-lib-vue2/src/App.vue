4709ec2ff06b3530d8c7e4cf46adf8b6fd88d880
monolith-hjlee <77139601+monolith-hjlee@users.noreply.github.com>
2021-06-02 09:54:02 +0000

    Vue2 로 재작성

---
+++ chart-lib-vue2/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Chart.js</h2>
+    <bar-chart></bar-chart>
+    <line-chart></line-chart>
+  </div>
+</template>
+
+<script>
+import BarChart from './components/BarChart.vue';
+import LineChart from './components/LineChart.vue';
+
+export default {
+  // 컴포넌트 속성 && 인스턴스 옵션
+  components: {
+    BarChart,
+    LineChart,
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
--- chart-lib/src/main.js
+++ chart-lib/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ChartPlugin from './plugins/ChartPlug.js';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(ChartPlugin);
+app.mount('#app')
